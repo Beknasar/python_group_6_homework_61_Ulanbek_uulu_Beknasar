681dde5e443f79bbf9cf8e94caea4a16cfa9b8f0
Beknasar <680633@gmail.com>
2020-08-26 23:17:10 +0600

    Поправлен баг с сохранением связи многие-ко-многим при сохранении формы с commit=False. Обновление задачи переделано на джанговский UpdateView.

--- source/webapp/views/task_views.py
+++ source/webapp/views/task_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from webapp.models import Tasks, Project
-from django.views.generic import View, TemplateView, FormView, ListView, CreateView
+from django.views.generic import View, TemplateView, ListView, CreateView, UpdateView
 
 from webapp.forms import TaskForm, SearchForm
 from django.db.models import Q, F
@@ -57,37 +57,18 @@ class TaskCreateView(CreateView):
         task = form.save(commit=False)
         task.project = project
         task.save()
+        form.save_m2m()
         return redirect('project_view', pk=project.pk)
 
 
-class UpdateView(FormView):
+class UpdateView(UpdateView):
     template_name = 'task/task_update.html'
     form_class = TaskForm
-
-    def get_object(self):
-        pk = self.kwargs.get('pk')
-        return get_object_or_404(Tasks, pk=pk)
-
-    def dispatch(self, request, *args, **kwargs):
-        self.task = self.get_object()
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['instance'] = self.task
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = self.task
-        return context
-
-    def form_valid(self, form):
-        self.task = form.save()
-        return super().form_valid(form)
+    model = Tasks
+    context_object_name = 'task'
 
     def get_success_url(self):
-        return reverse('task_view', kwargs={'pk': self.task.pk})
+        return reverse('task_view', kwargs={'pk': self.object.pk})
 
 
 class DeleteView(View):
