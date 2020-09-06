74ef77cae438b41756ec3e7175a667487cdd91bd
Beknasar <680633@gmail.com>
2020-09-06 22:02:34 +0600

    Добавлена валидация на поля first_name, last_name, email.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        errors = []
+        email = cleaned_data.get('email')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if not first_name:
+            if not last_name:
+                errors.append(ValidationError('You must fill in at least one of the specified fields: last_name, first_name'))
+        if not email:
+            errors.append(ValidationError('This field is required: email.'))
+        if errors:
+            raise ValidationError(errors)
+        return cleaned_data
