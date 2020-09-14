ad9e8bbd5c2001c3abd7f4d95cc0540cbda23ac3
Beknasar <680633@gmail.com>
2020-09-14 11:24:28 +0600

    Добавлена личная страница пользователя.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
 from accounts.forms import MyUserCreationForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 #
 # def login_view(request):
@@ -43,3 +46,22 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('index')
         return next_url
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    # paginate_related_by = 5
+    # paginate_related_orphans = 0
+
+    # def get_context_data(self, **kwargs):
+    #     articles = self.object.articles.order_by('-created_at')
+    #     paginator = Paginator(articles, self.paginate_related_by, orphans=self.paginate_related_orphans)
+    #     page_number = self.request.GET.get('page', 1)
+    #     page = paginator.get_page(page_number)
+    #     kwargs['page_obj'] = page
+    #     kwargs['articles'] = page.object_list
+    #     kwargs['is_paginated'] = page.has_other_pages()
+    #     return super().get_context_data(**kwargs)
+
