ce91cbc8d6255cc44ccdd1b44f213f00b4fee307
Beknasar <680633@gmail.com>
2020-09-14 12:14:37 +0600

    Добавлена миграция для создания профилей существующим пользователям и код для создания профиля после регистрации.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -2,13 +2,18 @@ from django.contrib.auth.forms import UserCreationForm, UsernameField
 from django.contrib.auth.models import User
 from django import forms
 from django.core.exceptions import ValidationError
-
+from accounts.models import Profile
 
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2',
                   'first_name', 'last_name', 'email']
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        Profile.objects.create(user=user)
+        return user
 
     def clean(self):
         cleaned_data = super().clean()
