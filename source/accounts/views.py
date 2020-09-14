0bbf4253ce5f2682d7444d148f0ebb18d010f066
Beknasar <680633@gmail.com>
2020-09-14 11:58:47 +0600

    Добавлен список проектов для пользователей, в которых они учавствуют.

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -52,16 +52,16 @@ class UserDetailView(LoginRequiredMixin, DetailView):
     model = get_user_model()
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
-    # paginate_related_by = 5
-    # paginate_related_orphans = 0
+    paginate_related_by = 5
+    paginate_related_orphans = 0
 
-    # def get_context_data(self, **kwargs):
-    #     articles = self.object.articles.order_by('-created_at')
-    #     paginator = Paginator(articles, self.paginate_related_by, orphans=self.paginate_related_orphans)
-    #     page_number = self.request.GET.get('page', 1)
-    #     page = paginator.get_page(page_number)
-    #     kwargs['page_obj'] = page
-    #     kwargs['articles'] = page.object_list
-    #     kwargs['is_paginated'] = page.has_other_pages()
-    #     return super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        projects = self.object.projects.order_by('-date_start')
+        paginator = Paginator(projects, self.paginate_related_by, orphans=self.paginate_related_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        kwargs['page_obj'] = page
+        kwargs['articles'] = page.object_list
+        kwargs['is_paginated'] = page.has_other_pages()
+        return super().get_context_data(**kwargs)
 
